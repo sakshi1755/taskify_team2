5fc74e57edd31a636289ed3c8d3337e75aabc891
Vishishta <pvishishta@gmail.com>
2025-06-21 13:24:45 +0530

    taskform is updated

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Taskform from './Components/Taskform';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,8 +29,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div>
+        <Taskform />
+      </div>
     </>
   )
 }
 
-export default App
+export default App;
